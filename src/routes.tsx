3949ef40b147c81dcac8f0ad8a85cd6ad882443c
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-09-26 15:32:11 -0300

    ♻️ refacto: remove BrowserRouter duplicado
    Remove o `BrowserRouter` de `AppRoutes`, deixando apenas o componente `Routes`.
    Isso evita a duplicação de roteadores caso o `Router` principal já seja definido em outro ponto da aplicação.

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,18 +1,16 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router'
+import { Route, Routes } from 'react-router'
 import Layout from '@/pages/Layout'
 import Catalog from '@/pages/Catalog'
 import BookDetail from '@/pages/BookDetail'
 
 const AppRoutes = () => {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Catalog />} />
-                    <Route path="book/:id" element={<BookDetail />} />
-                </Route>
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Catalog />} />
+                <Route path="book/:id" element={<BookDetail />} />
+            </Route>
+        </Routes>
     )
 }
 
